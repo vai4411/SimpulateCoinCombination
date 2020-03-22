bb739befd7c27cccd155cbe7946db7ec99e709bf
root <root@localhost.localdomain>
2020-03-22 23:07:28 +0000

    First Commit

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -1,5 +1,7 @@
+
 #!/bin/bash -x
 
+#Use case 1
 r=$((RANDOM % 2))
 if [ $r -eq 1 ]
 then
@@ -8,3 +10,23 @@ else
         echo "Tail wins"
 fi
 
+#Use case 2
+read -p "Enter number of flips:" n
+coin[head]=0
+coin[tail]=0
+for ((i=0; i<$n; i++))
+do
+	r=$((RANDOM % 2))
+	if [ $r -eq 1 ]
+	then
+		head=$(($head + 1))
+		coin[head]=$head
+	else
+		tail=$(($tail + 1))
+		coin[tail]=$tail
+	fi
+done
+headperc=`(echo 'scale=2;'${coin[head]}' / '$n' * 100' | bc)`
+echo "Percentage of head $headperc"
+tailperc=`(echo 'scale=2;'${coin[tail]}' / '$n' * 100' | bc)`
+echo "Percentage of head $tailperc"
